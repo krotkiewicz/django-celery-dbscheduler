4acd69351bf31e559ad7ef2752d05371bd695786
Konrad Rotkiewicz <konrad.rotkiewicz@gmail.com>
2015-03-02 10:20:28 +0100

    bump version

--- dbscheduler/__init__.py
+++ dbscheduler/__init__.py
@@ -3,7 +3,7 @@
 # :license:   BSD, see LICENSE for more details.
 from __future__ import absolute_import, unicode_literals
 
-VERSION = (3, 2, 0, '')
+VERSION = (3, 2, 1, '')
 __version__ = '.'.join(map(str, VERSION[0:3])) + ''.join(VERSION[3:])
 __author__ = ''
 __contact__ = ''
